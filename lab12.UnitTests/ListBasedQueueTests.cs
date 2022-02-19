14665668cec9a504fd613522c540fccf8aba0b4f
Halilov Artur <therock65frostik@gmail.com>
2022-02-19 09:48:14 +0500

    code coverage added

--- lab12.UnitTests/ListBasedQueueTests.cs
+++ lab12.UnitTests/ListBasedQueueTests.cs
@@ -52,6 +52,31 @@ namespace lab12.UnitTests
             Assert.AreEqual(result, expected);
 
         }
+
+        [TestMethod]
+        public void Clone_ReturnEqualElemsAndDifferentRefsTrue()
+        {
+            // Arrange
+            bool expected = true;
+            // Act
+            ListBasedQueue<Person> q = new ListBasedQueue<Person>();
+            q.Push(new Person("Anton"));
+            q.Push(new Worker("Dima"));
+            ListBasedQueue<Person> q1 = (ListBasedQueue<Person>)q.Clone();
+            bool result = true;
+            while(q1.head != null)
+            {
+                if (q1.head == q.head || q.head.data.ToString() != q1.head.data.ToString()) result = false;
+                q1.head = q1.head.next;
+                q.head = q.head.next;
+            }
+
+            // Assert
+            Assert.AreEqual(result, expected);
+
+        }
+
+
         [TestMethod]
         public void DeleteQueue_ReturnNull()
         {
@@ -97,6 +122,24 @@ namespace lab12.UnitTests
             Assert.AreEqual(result, expected);
 
         }
+
+        [TestMethod]
+        public void PushtoNonEmptyQueue_ReturnTwoElemAntonTrue()
+        {
+            // Arrange
+            bool expected = true;
+            // Act
+            ListBasedQueue<Person> q = new ListBasedQueue<Person>();
+            q.Push(new Person("Jora"));
+            q.Push(new Worker("Ben"));
+            q.Push(new Person("Anton"));
+
+
+            bool result = q.head.data.ToString() == "Jora" && q.head.next.data.ToString() == "Ben" && q.head.next.next.data.ToString() == "Anton";
+            // Assert
+            Assert.AreEqual(result, expected);
+
+        }
         [TestMethod]
         public void PushSome_ReturnTwoElemsAntonAndIgorTrue()
         {
@@ -108,6 +151,40 @@ namespace lab12.UnitTests
 
 
             bool result = q.Count == 2 && q.head.data.ToString() == "Anton" && q.head.next.data.ToString() == "Igor";
+            // Assert
+            Assert.AreEqual(result, expected);
+
+        }
+        [TestMethod]
+        public void PushSomeNonEmptyQueueHeadAndNextAlready_ReturnTwoElemsAntonAndIgorTrue()
+        {
+            // Arrange
+            bool expected = true;
+            // Act
+            ListBasedQueue<Person> q = new ListBasedQueue<Person>();
+            q.Push(new Person("Bill"));
+            q.Push(new Person("Sasha"));
+            q.Push(new Person[] { new Person("Anton"), new Person("Igor") });
+
+
+            bool result = q.Count == 4 &&  q.head.data.ToString() == "Bill" && q.head.next.data.ToString() == "Sasha" && q.head.next.next.data.ToString() == "Anton" && q.head.next.next.next.data.ToString() == "Igor";
+            // Assert
+            Assert.AreEqual(result, expected);
+
+        }
+
+        [TestMethod]
+        public void PushSomeNonEmptyQueue_ReturnTwoElemsAntonAndIgorTrue()
+        {
+            // Arrange
+            bool expected = true;
+            // Act
+            ListBasedQueue<Person> q = new ListBasedQueue<Person>();
+            q.Push(new Person("Bill"));
+            q.Push(new Person[] { new Person("Anton"), new Person("Igor") });
+
+
+            bool result = q.Count == 3 && q.head.data.ToString() == "Bill" && q.head.next.data.ToString() == "Anton" && q.head.next.next.data.ToString() == "Igor";
             // Assert
             Assert.AreEqual(result, expected);
 
